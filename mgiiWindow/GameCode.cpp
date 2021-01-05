3e1242666d7a43eb425ef35f7a713a296b75a107
rumothy <rumothy@yahoo.com>
2021-01-05 08:46:28 -0500

    InitInstance 1

--- mgiiWindow/GameCode.cpp
+++ mgiiWindow/GameCode.cpp
@@ -10,5 +10,14 @@ GameCodeApp::GameCodeApp()
 
 bool GameCodeApp::InitInstance(HINSTANCE hInstance, LPWSTR lpCmdLine, HWND hWnd, int screenWidth, int screenHeight)
 {
+	// Check for existing instance of the same window
+	//
+#ifndef _DEBUG
+	// Note - it can be really useful to debug network code to have
+	// more thatn one instance of the game up at one time - so 
+	// fell free to dcomment these lines in or out as  you wish!
+	if (!IsOnlyInstance(VGetGameTitle()))
+		return false;
+#endif
 	return TRUE;
 }
--- mgiiWindow/GameCode.h
+++ mgiiWindow/GameCode.h
@@ -7,6 +7,12 @@ class GameCodeApp
 public:
 	GameCodeApp();
 
+	// Game Application Data
+	// You must define these in an inherited
+	// class - see TeapotWarsApp for an example
+
+	virtual TCHAR* VGetGameTitle() = 0;
+
 	virtual bool InitInstance(HINSTANCE hInstance, LPWSTR lpCmdLine, HWND hWnd = NULL, 
 		int screenWidth = SCREEN_WIDTH, int screenHeight = SCREEN_HEIGHT);
 	struct GameOptions m_Options;
